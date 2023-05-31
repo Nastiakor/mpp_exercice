8fd0750cc263b9f7e8af63b72d4847b3e377d8c7
Nastiakor <a.korotkova@yahoo.fr>
2023-05-31 17:46:35 +0200

    function name change

--- lib/widgets/contact_card.dart
+++ lib/widgets/contact_card.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
-import 'package:mpp/utils/toast.dart';
+import 'package:mpp/utils/copy_phone_number.dart';
 import 'package:mpp/style.dart';
 
 class ContactCard extends StatelessWidget {
